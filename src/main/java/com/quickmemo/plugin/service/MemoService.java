ba6ecbe7e7ca0f4e284eb21cd2d8fec73d636d19
binary-ho <binaryhoho@gmail.com>
2025-01-27 19:42:04 +0900

    feat: 기본 문구 추가

--- src/main/java/com/quickmemo/plugin/service/MemoService.java
+++ src/main/java/com/quickmemo/plugin/service/MemoService.java
@@ -11,10 +11,22 @@ import org.jetbrains.annotations.Nullable;
     storages = {@Storage("quickMemo.xml")}
 )
 public final class MemoService implements PersistentStateComponent<MemoService.State> {
-    private State state = new State();
+    private static final String DEFAULT_MEMO = """
+            Free your Brain, Just Memo It\n
+            생각을 뇌에서 놓아주세요. 그냥 메모하세요\n\n
+            default-keymap\n
+            - macOS: Command + Option + M\n
+            - windows/linux: Ctrl + M\n
+        """;
+
+    private State state;
 
     public static class State {
-        public String content = "";
+        public String content = DEFAULT_MEMO;
+    }
+
+    public MemoService() {
+        this.state = new State();
     }
 
     public static MemoService getInstance(@NotNull Project project) {
