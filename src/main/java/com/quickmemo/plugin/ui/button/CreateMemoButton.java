349048845e9b60f3cc8418ce2ec6daa98c036917
binary_ho <dfghcvb11@naver.com>
2025-02-05 20:55:42 +0900

    refactor: replace created memo toast to dialog ㅜㅜ (#14)
    * refactor: 기존 Toast 삭제 ㅠㅠ
    
    * refactor: 생성 알림 dialog 구현
    
    * refactor: rename package message to dialog

--- src/main/java/com/quickmemo/plugin/ui/button/CreateMemoButton.java
+++ src/main/java/com/quickmemo/plugin/ui/button/CreateMemoButton.java
@@ -10,7 +10,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.InputEvent;
 import java.awt.event.MouseEvent;
-import java.util.function.Consumer;
 
 public class CreateMemoButton extends ActionButton {
     private final AnAction createMemoAction;
@@ -19,32 +18,30 @@ public class CreateMemoButton extends ActionButton {
     private static final String NEW_MEMO_BUTTON_DESCRIPTION = "Create new memo";
     private static final Icon NEW_MEMO_BUTTON_ICON = AllIcons.General.Add;
 
-    public CreateMemoButton(Runnable createAction, Consumer<Component> onCreateSuccess, Runnable onCreateFailed) {
+    public CreateMemoButton(Runnable createAction, Runnable onCreateSuccess, Runnable onCreateFailed) {
         this.createMemoAction = createAction(createAction, onCreateSuccess, onCreateFailed);
     }
 
-    private AnAction createAction(Runnable createAction, Consumer<Component> listener, Runnable actionFaild) {
+    private AnAction createAction(Runnable createAction, Runnable onCreateSuccess, Runnable onCreateFailed) {
         return new AnAction(NEW_MEMO_BUTTON_NAME, NEW_MEMO_BUTTON_DESCRIPTION, NEW_MEMO_BUTTON_ICON) {
             @Override
             public void actionPerformed(@NotNull AnActionEvent event) {
                 try {
                     createAction.run();
-                    acceptIfMouseEvent(event, listener);
+                    acceptIfMouseEvent(event, onCreateSuccess);
                 } catch (MemoLimitExceededException exception) {
-                    actionFaild.run();
+                    onCreateFailed.run();
                 }
             }
         };
     }
 
-    private void acceptIfMouseEvent(@NotNull AnActionEvent event, Consumer<Component> listener) {
+    private void acceptIfMouseEvent(@NotNull AnActionEvent event, Runnable onCreateSuccess) {
         InputEvent inputEvent = event.getInputEvent();
         if (!(inputEvent instanceof MouseEvent)) {
             return;
         }
-
-        Component component = inputEvent.getComponent();
-        listener.accept(component);
+        onCreateSuccess.run();
     }
 
     @Override
---
+++ src/main/java/com/quickmemo/plugin/ui/dialog/CreatedMemoDialog.java
@@ -0,0 +1,15 @@
+package com.quickmemo.plugin.ui.dialog;
+
+import com.intellij.openapi.ui.Messages;
+
+public class CreatedMemoDialog {
+    private static final String CREATED_MEMO_MESSAGE = "Memo Created!";
+    private static final String CREATED_MEMO_TITLE = "";
+
+    public static void show() {
+        Messages.showInfoMessage(
+                CREATED_MEMO_MESSAGE,
+                CREATED_MEMO_TITLE
+        );
+    }
+}
