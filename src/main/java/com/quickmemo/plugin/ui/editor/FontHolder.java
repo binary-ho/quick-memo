b0b190ef265b48c484084540ab47d9f8bfd6ac7a
binary-ho <dfghcvb11@naver.com>
2025-02-06 21:09:25 +0900

    refactor: 현재 사용 가능한 한글 폰트를 우선적으로 적용하도록 변경

--- src/main/java/com/quickmemo/plugin/ui/editor/FontHolder.java
+++ src/main/java/com/quickmemo/plugin/ui/editor/FontHolder.java
@@ -4,25 +4,26 @@ import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.editor.colors.EditorColorsManager;
 import com.intellij.openapi.editor.colors.EditorColorsScheme;
 import com.intellij.openapi.editor.colors.EditorFontType;
-import com.intellij.util.ui.UIUtil;
 
 import java.awt.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class FontHolder {
     private static final Font DEFAULT_GLOBAL = new Font(Font.MONOSPACED, Font.PLAIN, 14);
-    public static final String DEFAULT_KOREAN_FONT = Font.DIALOG;
-    private static final String[] KOREAN_FONTS = {
+    private static final List<String> KOREAN_FONTS = List.of(
             // for macOS
             "Apple SD GothicNeo Neo",
             "AppleSDGothicNeo-Regular",
 
             // for Window guys
-            "Malgun Gothic",
+            "D2Coding",
+            "맑은 고딕",
 
             // Linux
             "NotoSansCJK-Regular",
             "Noto Sans CJK KR"
-    };
+    );
 
     public static final char VALIDATE_KOREAN_CHAR = '한';
 
@@ -30,46 +31,41 @@ public class FontHolder {
         Font editorFont = getIDEFont();
 
         // find korean for korean users..
-        String koreanFont = findKoreanFont();
-
+        String koreanFont = getKoreanFont();
         String fontFamily = editorFont.getFamily() + ", " + koreanFont;
         return Font.decode(fontFamily + "-PLAIN-" + editorFont.getSize());
     }
 
+    private String getKoreanFont() {
+        String koreanFont =  findKoreanFont();
+        System.out.println("[DEBUG] Korean font: " + koreanFont);
+        if (koreanFont != null) {
+            return koreanFont;
+        }
+        return DEFAULT_GLOBAL.getFamily();
+    }
+
     private String findKoreanFont() {
         if (KOREAN_FONTS == null) {
-            return DEFAULT_KOREAN_FONT;
+            return DEFAULT_GLOBAL.getFamily();
         }
 
-        for (String koreanFont : KOREAN_FONTS) {
-            try {
-                if (canDisplay(koreanFont)) {
-                    return koreanFont;
-                }
-            } catch (Exception e) {
-                return getDefaultKoreanFont();
-            }
+        String installedKoreanFont = findInstalledKoreanFont();
+        if (installedKoreanFont != null) {
+            return installedKoreanFont;
         }
-
-        return getDefaultKoreanFont();
+        return DEFAULT_GLOBAL.getFamily();
     }
 
-    private String getDefaultKoreanFont() {
-        try {
-            Font ideFont = UIUtil.getFontWithFallback(DEFAULT_KOREAN_FONT, Font.PLAIN, 1);
-            if (ideFont.canDisplay(VALIDATE_KOREAN_CHAR)) {
-                return ideFont.getFamily();
-            } else {
-                return DEFAULT_KOREAN_FONT;
-            }
-        } catch (Exception e) {
-            return DEFAULT_KOREAN_FONT;
-        }
-    }
-
-    private boolean canDisplay(String koreanFont) {
-        Font font = new Font(koreanFont, Font.PLAIN, 1);
-        return font.canDisplay(VALIDATE_KOREAN_CHAR);
+    private String findInstalledKoreanFont() {
+        GraphicsEnvironment graphicsEnvironment = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        String[] availableFontFamilyNames = graphicsEnvironment.getAvailableFontFamilyNames();
+        List<String> installedFonts = Arrays.asList(availableFontFamilyNames);
+        return installedFonts.stream()
+                .filter(KOREAN_FONTS::contains)
+                .filter(this::canDisplay)
+                .findFirst()
+                .orElse(getGlobalFont().getFamily());
     }
 
     private Font getIDEFont() {
@@ -91,5 +87,10 @@ public class FontHolder {
         }
         EditorColorsScheme globalScheme = colorsManager.getGlobalScheme();
         return globalScheme.getFont(EditorFontType.PLAIN);
+    }
+
+    private boolean canDisplay(String koreanFont) {
+        Font font = new Font(koreanFont, Font.PLAIN, 1);
+        return font.canDisplay(VALIDATE_KOREAN_CHAR);
     }
 }
